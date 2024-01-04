583909f932e57332498ed097e33d9b2431aa7c1f
feps <120133472+fepskieee@users.noreply.github.com>
2024-01-05 07:15:17 +0800

    feat: Added username for each users in message chat.

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -5,13 +5,14 @@ const ChatMessage = ({ message }) => {
       {/* <img
         src={`https://i.pravatar.cc/40?u=${sender}`}
         alt={sender}
-        className="w-10 h-10 rounded-full mr-3"
+        className="w-10 h-10 mr-3 rounded-full"
       /> */}
       <div className="flex flex-col">
-        <div className="bg-gray-200 rounded-lg px-4 py-2">
-          <p className="text-sm text-gray-800">{`${message.id}: ${message.message}`}</p>
+        <p className="text-xs">{message.id}</p>
+        <div className="px-4 py-2 bg-gray-200 rounded-lg">
+          <p className="text-sm text-gray-800">{`${message.message}`}</p>
         </div>
-        {/* <span className="text-xs text-gray-300 mt-1">{time}</span> */}
+        {/* <span className="mt-1 text-xs text-gray-300">{time}</span> */}
       </div>
     </div>
   );
